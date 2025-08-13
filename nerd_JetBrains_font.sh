e164a74a5428ae24f321f58b35bf2e85d4f85fdd
ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
2025-08-13 14:24:29 -0400

    fix: Didn't Reload Font, Didn't Use Sudo

--- nerd_JetBrains_font.sh
+++ nerd_JetBrains_font.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep "JetBrainsMono.tar.xz" | cut -d : -f 2,3 | tr -d \" | wget -i - -O /tmp/NerdJetBrains.tar.xz
-mkdir -p /usr/local/share/fonts/NerdJetBrains/ 
-tar -xvf /tmp/NerdJetBrains.tar.xz -C /usr/local/share/fonts/NerdJetBrains/ 
+sudo mkdir -p /usr/local/share/fonts/NerdJetBrains/
+sudo tar -xvf /tmp/NerdJetBrains.tar.xz -C /usr/local/share/fonts/NerdJetBrains/
+sudo fc-cache -f
