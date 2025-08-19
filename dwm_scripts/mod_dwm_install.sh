c50dd9687caf76833a6d623c851649155ed132f8
ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
2025-08-19 03:25:34 -0400

    refactor: Remove xdm And Uses .bash_login Instead

--- dwm_scripts/mod_dwm_install.sh
+++ dwm_scripts/mod_dwm_install.sh
@@ -4,7 +4,7 @@ dwmflexipatch_path="/tmp/dwm-flexipatch"
 patches_location="./patches"
 
 sudo apt install nala -y
-sudo nala install xorg dmenu xdm thunar alacritty pulseaudio alsamixergui lynx rofi -y
+sudo nala install xorg dmenu thunar alacritty pulseaudio alsamixergui lynx rofi -y
 
 ./set_max_volume.sh
 
@@ -17,16 +17,18 @@ read -p "Do you want to delete packages used for compiling dwm to save space? [y
 echo "(git won't be deleted cause probly gonna be used a lot)"
 
 if [ "$user_prompt" == "y" ]; then
-    sudo nala remove make gcc libx11-dev libxft-dev libxinerama-dev -y
+  sudo nala remove make gcc libx11-dev libxft-dev libxinerama-dev -y
 fi
 
 # Apply Patches
 sudo cp $patches_location/patches.h $dwmflexipatch_path/patches.h
 sudo cp $patches_location/config.h $dwmflexipatch_path/config.h
 
-# Add dwm session to xdm
-echo "exec dwm" > ~/.xsession
-echo "exec dwm" > ~/.xinitrc #Fallback incase .xsession doesn't work
+# Run dwm session when executing 'startx'
+echo "exec dwm" >~/.xsession
+echo "exec dwm" >~/.xinitrc #Fallback incase .xsession doesn't work
+
+echo "startx" >~/.bash_login
 
 # Start Compiling
 cd $dwmflexipatch_path
