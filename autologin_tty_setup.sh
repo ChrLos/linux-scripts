3c71120d58bea79c06bc7d7aea5ee7ad2025beb3
ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
2025-09-01 11:22:36 -0400

    feat: Autologin Auto Setup

---
+++ autologin_tty_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo nala install mingetty
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
+
+read -p "What is the username that you want to autologin?: " username
+cat >/etc/systemd/system/getty@tty1.service.d/override.conf <<EOF
+[Service]
+ExecStart=
+ExecStart=-/sbin/mingetty --autologin $username --noclear tty1
+EOF
+
+sudo systemctl enable getty@tty1.service
+
