5e4326898484a654671121d8078e97a1e23e1c42
ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
2025-09-07 22:54:06 -0400

    fix(tty_autologin): No Permission When Creating File

--- autologin_tty_setup.sh
+++ autologin_tty_setup.sh
@@ -4,11 +4,11 @@ sudo nala install mingetty
 sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
 
 read -p "What is the username that you want to autologin?: " username
-cat >/etc/systemd/system/getty@tty1.service.d/override.conf <<EOF
+sudo bash -c "cat >/etc/systemd/system/getty@tty1.service.d/override.conf <<EOF
 [Service]
 ExecStart=
 ExecStart=-/sbin/mingetty --autologin $username --noclear tty1
-EOF
+EOF"
 
 sudo systemctl enable getty@tty1.service
 
