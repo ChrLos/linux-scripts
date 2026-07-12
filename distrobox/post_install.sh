35f5762dfa333cbad3b253b66ae578c194bdfb6b
ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
2026-07-12 10:37:47 -0400

    feat: Distrobox Assemble Automation Squashed commit of the following:
    commit fded4c73bbd4d544b9b1b62156ac369e46a49493
    Author: ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
    Date:   Mon Jul 6 02:14:48 2026 -0400
    
        feat: Export The Apps After Installing
    
    commit d2d1ed6b3c9d43e660b4e23505f0bd123c1bad6b
    Author: ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
    Date:   Mon Jul 6 01:25:23 2026 -0400
    
        docs: Add Comment Documentation
    
    commit b6d7d51bf2b4d01ca88667cd085d961c51b31d71
    Author: ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
    Date:   Mon Jul 6 01:24:03 2026 -0400
    
        fix: Fixing typos
    
    commit e3c9de515fc6d5e08d85f4f02a3ac877b87787d2
    Author: ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
    Date:   Mon Jul 6 01:02:56 2026 -0400
    
        fix: Error fixes, Change Path Location, Add -y
    
    commit 003b3b3a436224f2514305be29fabdaf1d7f2e77
    Author: ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
    Date:   Sat Jul 4 14:59:33 2026 -0400
    
        fix: Adding FLAVOR for each container
    
    commit 327f3f9a8a12efdf7503ae39756090b951dbbd49
    Author: ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
    Date:   Sat Jul 4 14:45:38 2026 -0400
    
        feat: Distrobox Automation

---
+++ distrobox/post_install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+first_install="FALSE" # Change to TRUE before using it, change back to FALSE after use
+# Not changing to TRUE will cause ERROR in init_hooks, just do 'distrobox assemble create --replace'
+FLAVOR="${FLAVOR:-default}"
+
+if [[ "$first_install" == "FALSE" || "$FLAVOR" == "default" ]]; then
+  exit 1
+fi
+
+coding() {
+  # Brave Origin
+  curl -fsS https://dl.brave.com/install.sh | FLAVOR=origin CHANNEL=nightly sh
+
+  # VS Code
+  sudo apt install wget gpg -y && wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft.gpg
+  sudo tee /etc/apt/sources.list.d/vscode.sources <<'EOF'
+Types: deb
+URIs: https://packages.microsoft.com/repos/code
+Suites: stable
+Components: main
+Architectures: amd64,arm64,armhf
+Signed-By: /usr/share/keyrings/microsoft.gpg
+EOF
+  sudo apt update && sudo apt install code -y
+}
+
+browser() {
+  # Brave Origin
+  curl -fsS https://dl.brave.com/install.sh | FLAVOR=origin CHANNEL=nightly sh
+
+  # Mullvad Browser
+  sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
+  echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$(dpkg --print-architecture)] https://repository.mullvad.net/deb/stable stable main" | sudo tee /etc/apt/sources.list.d/mullvad.list
+  sudo apt update
+  sudo apt install mullvad-browser -y
+}
+
+lazyvim() {
+  # required
+  mv ~/.config/nvim{,.bak}
+
+  # optional but recommended
+  mv ~/.local/share/nvim{,.bak}
+  mv ~/.local/state/nvim{,.bak}
+  mv ~/.cache/nvim{,.bak}
+
+  git clone https://github.com/LazyVim/starter ~/.config/nvim
+
+  rm -rf ~/.config/nvim/.git
+}
+
+case "$FLAVOR" in
+coding) coding ;;
+browser) browser ;;
+lazyvim) lazyvim ;;
+esac
